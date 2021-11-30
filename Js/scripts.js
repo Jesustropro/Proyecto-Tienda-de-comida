ceb54eb9059a1a9d22b5aab2c6b87bba773cd08a
Jesustropro <jdmoraelpro1@gmail.com>
2021-11-30 19:20:39 -0400

    cambios en el diseño general, creacion de ventana con informacion del cliente y su pedido al dar click a finalizar el pedido

--- Js/scripts.js
+++ Js/scripts.js
@@ -7,7 +7,6 @@ let panDeJamon = 10
 let carrito = 0
 let comida = ""
 
-
 class Comida{                                
     constructor(nombre, precio){
     this.Nombre = nombre;
@@ -31,7 +30,7 @@ let arrayCliente = []
 let botonEnviar = document.getElementById("enviarDatos")
 let formCliente = document.getElementById("formCliente")
 
-formCliente = addEventListener("submit", (e) => {
+formCliente = addEventListener("submit", (e) => {    //formulario para obtener informacion del cliente
     e.preventDefault()
     let nombreCliente = document.getElementById("nombre").value
     let direccionCliente = document.getElementById("direccion").value
@@ -41,43 +40,43 @@ formCliente = addEventListener("submit", (e) => {
  
 let botonHamburguesa = document.getElementById("boton_comprar_hamburguesa")
 
-botonHamburguesa.addEventListener("click", () => {
+botonHamburguesa.addEventListener("click", () => { //alerta de pedido exitoso
     carrito = hamburguesa + carrito
     comida= comida + "1 hamburguesa + "
     let contenedor = document.createElement("div")
-    contenedor.innerHTML = `<div class="alert alert-success d-flex align-items-center position-relative position-absolute top-0 end-0 boton" role="alert">
-    <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Success:"><use xlink:href="#check-circle-fill"/></svg>
+    contenedor.innerHTML = `<div class="alert alert-success alert-dismissible fade show position-absolute top-0 end-0 boton" role="alert">
     <div id="div">
       <p id="parrafo">Añadido al carrito exitosamente</p>
-    </div>`
+    </div>
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>`
     document.body.appendChild(contenedor)
 })
 
 let botonPizza = document.getElementById("boton_comprar_pizza")
 
-botonPizza.addEventListener("click",() => {
+botonPizza.addEventListener("click",() => {  //alerta de pedido exitoso
     carrito = pizza + carrito
     comida= comida + "1 pizza + "
     let contenedor = document.createElement("div")
-    contenedor.innerHTML = `<div class="alert alert-warning d-flex align-items-center position-relative position-absolute top-0 end-0 boton" role="alert">
-    <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Success:"><use xlink:href="#check-circle-fill"/></svg>
+    contenedor.innerHTML = `<div class="alert alert-warning alert-dismissible fade show position-absolute top-0 end-0 boton" role="alert">
     <div id="div">
       <p id="parrafo">Añadido al carrito exitosamente</p>
-    </div>`
+    </div>
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>`
     document.body.appendChild(contenedor)
 })
 
 let botonPan = document.getElementById("boton_comprar_pan")
 
-botonPan.addEventListener("click", () => {
+botonPan.addEventListener("click", () => {  //alerta de pedido exitoso
     carrito = panDeJamon + carrito
     comida= comida + "1 pan de jamon + "
     let contenedor = document.createElement("div")
-    contenedor.innerHTML = `<div class="alert alert-danger d-flex align-items-center position-relative position-absolute top-0 end-0 boton" role="alert">
-    <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Success:"><use xlink:href="#check-circle-fill"/></svg>
+    contenedor.innerHTML = `<div class="alert alert-danger alert-dismissible fade show position-absolute top-0 end-0 boton" role="alert">
     <div id="div">
       <p id="parrafo">Añadido al carrito exitosamente</p>
-    </div>`
+    </div>
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>`
     document.body.appendChild(contenedor)
 })
 
@@ -100,18 +99,25 @@ let precioFinal = resultado(precio)
 carrito = precioFinal          
 precioFinal = precioFinal + "$" /* Precio final del pedido que se le muestra al cliente */
 
-let contenedorPedidoFinal = document.createElement("div")
-contenedorPedidoFinal.innerHTML = `<h3>Su pedido es:</h3>
-                       <h4>${comida}</h4>
-                       <h3>El costo final de su pedido es:
-                       <h4>${precioFinal}
-                       
-                       <div class="alert alert-primary d-flex align-items-center position-relative position-absolute top-0 end-0 boton" role="alert">
-                       <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Success:"><use xlink:href="#check-circle-fill"/></svg>
-                       <div id="div">
-                         <p id="parrafo">Su pedido se ha realizado con éxito, disfrute su comida!</p>
-                       </div>`                       
-document.body.appendChild(contenedorPedidoFinal)
-console.log("El cliente ha hecho un pedido de " + comida + "con un costo total de " + precioFinal)
+let contenedor = document.createElement("div") //Ventana modal con información para el cliente
+contenedor.innerHTML = `<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal-dialog">
+  <div class="modal-content">
+    <div class="modal-header">
+      <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+    </div>
+    <div class="modal-body">
+      <p>Usted ha realizado un pedido de: ${comida}</p>
+      <p>El valor total a pagar por su pedido es: ${precioFinal}</p>     
+      <p>Disfrute de su comida!</p>
+    </div>
+    <div class="modal-footer">
+      <button type="button" class="btn btn-warning" data-bs-dismiss="modal">Close</button>
+      <button type="button" class="btn btn-danger">Realizar pago</button>
+    </div>
+  </div>
+</div>
+</div>`
+document.body.appendChild(contenedor) 
 })
-
